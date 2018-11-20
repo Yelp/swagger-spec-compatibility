e3a39ec4ba8aacc1aca91eb93deaec7a52885859
Samuele Maci <macisamuele@gmail.com>
2018-11-20 13:39:42 +0100

    RuleRegistry store rules by error code

--- swagger_spec_compatibility/rules/common.py
+++ swagger_spec_compatibility/rules/common.py
@@ -23,19 +23,41 @@ class RuleRegistry(ABCMeta):
 
     @classmethod
     def _validate_class_attributes(mcs, cls):
-        # type: (typing.Type['RuleRegistry']) -> None
+        # type: (typing.Type['BaseRule']) -> None
         assert getattr(cls, 'error_code', None) is not None, 'error_code is a required class attribute for {}'.format(cls)
         assert getattr(cls, 'short_name', None) is not None, 'short_name is a required class attribute for {}'.format(cls)
         assert getattr(cls, 'description', None) is not None, 'description is a required class attribute for {}'.format(cls)
         assert getattr(cls, 'error_level', None) is not None, 'error_level is a required class attribute for {}'.format(cls)
 
+    @classmethod
+    def _prevent_rule_duplication(mcs, cls):
+        # type: (typing.Type['BaseRule']) -> None
+        assert cls.error_code not in RuleRegistry._REGISTRY, \
+            'Rule {} is already defined. Already existing: {} Trying to add: {}'.format(
+                cls.error_code,
+                RuleRegistry._REGISTRY[cls.error_code],
+                cls,
+            )
+
+    @classmethod
+    def _prevent_metaclass_usage_from_not_BaseRule_extensions(mcs, cls):
+        # type: (typing.Type) -> None
+        cls_fully_qualified_name = '{}.{}'.format(cls.__module__, cls.__name__)
+        base_rule_fully_qualified_name = '{}.BaseRule'.format(mcs.__module__)
+        assert (  # pragma: no branch
+            cls_fully_qualified_name == base_rule_fully_qualified_name or
+            any(base_rule_fully_qualified_name == '{}.{}'.format(c.__module__, c.__name__) for c in cls.__bases__)
+        ), '{} metaclass should be used only by {}.BaseRule'.format(mcs, mcs.__module__)
+
     def __new__(mcs, name, bases, namespace):
         # type: (typing.Type['RuleRegistry'], str, typing.Tuple[type, ...], typing.Dict[str, typing.Any]) -> type
-        new_cls = ABCMeta.__new__(mcs, name, bases, namespace)
+        new_cls = ABCMeta.__new__(mcs, name, bases, namespace)  # type: typing.Type['BaseRule']
+        mcs._prevent_metaclass_usage_from_not_BaseRule_extensions(new_cls)
 
         if name != 'BaseRule':
             mcs._validate_class_attributes(new_cls)
-            RuleRegistry._REGISTRY[name] = new_cls
+            mcs._prevent_rule_duplication(new_cls)
+            RuleRegistry._REGISTRY[new_cls.error_code] = new_cls
         return new_cls
 
     @staticmethod
