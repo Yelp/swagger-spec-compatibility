a12c5d4bf081954005efd3fe77c8a7ca471529f9
Samuele Maci <macisamuele@gmail.com>
2018-11-27 14:12:06 +0100

    Add test to ensure that backward incompatibility testers are valid

--- tests/docs_test.py
+++ tests/docs_test.py
@@ -5,34 +5,13 @@ from __future__ import unicode_literals
 
 import os
 import re
+import sys
 from glob import iglob
 from itertools import chain
+from subprocess import CalledProcessError
+from subprocess import check_call
 
 import pytest
-
-
-@pytest.fixture(scope='session')
-def repository_root():
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-
-
-@pytest.fixture(scope='session')
-def docs_dir(repository_root):
-    print(repository_root)
-    return os.path.join(repository_root, 'docs', 'source')
-
-
-@pytest.fixture(scope='session')
-def package_root(repository_root):
-    return os.path.join(repository_root, 'swagger_spec_compatibility')
-
-
-@pytest.fixture(scope='session')
-def source_files(package_root):
-    return set(chain(
-        iglob(os.path.join(package_root, '*.py')),
-        iglob(os.path.join(package_root, '**', '*.py')),
-    ))
 
 
 BLACKLIST_MODULES_FROM_DOCUMENTATION = {
@@ -40,10 +19,19 @@ BLACKLIST_MODULES_FROM_DOCUMENTATION = {
 }
 
 
-def _from_path_to_module(package_root, path):
+def _extract_documented_models(doc_file):
+    regex = re.compile('\\.\\.\\sautomodule::\\s+(?P<module>[^\\s]+)')
+    with open(doc_file) as f:
+        for line in f.readlines():
+            m = regex.search(line)
+            if m:
+                yield m.group(str('module'))
+
+
+def _from_path_to_module(PACKAGE_DIR, path):
     return '{lib}.{module}'.format(
-        lib=os.path.basename(package_root),
-        module=os.path.relpath(path, package_root).replace('.py', '').replace(os.sep, '.'),
+        lib=os.path.basename(PACKAGE_DIR),
+        module=os.path.relpath(path, PACKAGE_DIR).replace('.py', '').replace(os.sep, '.'),
     ).replace('.__init__', '')
 
 
@@ -61,46 +49,36 @@ def _contain_docstring_or_code(path):
         return any(not_empty_lines)
 
 
-@pytest.fixture(scope='session')
-def python_modules(package_root, source_files):
-    modules = {
-        _from_path_to_module(package_root, source_file)
-        for source_file in source_files
-        if _contain_docstring_or_code(source_file)
-    }
-    return modules - BLACKLIST_MODULES_FROM_DOCUMENTATION
+REPOSITORY_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+DOC_DIR = os.path.join(REPOSITORY_ROOT, 'docs', 'source')
+PACKAGE_DIR = os.path.join(REPOSITORY_ROOT, 'swagger_spec_compatibility')
+SOURCE_FILES = set(chain(
+    iglob(os.path.join(PACKAGE_DIR, '*.py')),
+    iglob(os.path.join(PACKAGE_DIR, '**', '*.py')),
+))
+PYTHON_MODULES = {
+    _from_path_to_module(PACKAGE_DIR, source_file)
+    for source_file in SOURCE_FILES
+    if _contain_docstring_or_code(source_file)
+} - BLACKLIST_MODULES_FROM_DOCUMENTATION
+DOC_FILES = set(chain(
+    iglob(os.path.join(DOC_DIR, '*.md')),
+    iglob(os.path.join(DOC_DIR, '**', '**.md')),
+    iglob(os.path.join(DOC_DIR, '*.rst')),
+    iglob(os.path.join(DOC_DIR, '**', '**.rst')),
+))
+DOCUMENTED_MODULES = {
+    documented_module
+    for file in DOC_FILES
+    for documented_module in _extract_documented_models(file)
+}
+BACKWARD_INCOMPATIBILITY_TESTERS = sorted(
+    iglob(os.path.join(DOC_DIR, 'rules', 'examples', '*', 'tester.py')),
+)
 
 
-@pytest.fixture(scope='session')
-def doc_files(docs_dir):
-    return set(chain(
-        iglob(os.path.join(docs_dir, '*.md')),
-        iglob(os.path.join(docs_dir, '**', '**.md')),
-        iglob(os.path.join(docs_dir, '*.rst')),
-        iglob(os.path.join(docs_dir, '**', '**.rst')),
-    ))
-
-
-def get_documented_modules(doc_file):
-    regex = re.compile('\\.\\.\\sautomodule::\\s+(?P<module>[^\\s]+)')
-    with open(doc_file) as f:
-        for line in f.readlines():
-            m = regex.search(line)
-            if m:
-                yield m.group(str('module'))
-
-
-@pytest.fixture(scope='session')
-def documented_modules(doc_files):
-    return {
-        documented_module
-        for file in doc_files
-        for documented_module in get_documented_modules(file)
-    }
-
-
-def test_ensure_all_modules_are_available_in_documentation(python_modules, documented_modules):
-    not_documented_modules = python_modules - documented_modules
+def test_ensure_all_modules_are_available_in_documentation():
+    not_documented_modules = PYTHON_MODULES - DOCUMENTED_MODULES
     assert not not_documented_modules, \
         'The following modules are not available in documentation. ' \
         'Add them to documentation or to BLACKLIST_MODULES_FROM_DOCUMENTATION: {}'.format(
@@ -108,10 +86,15 @@ def test_ensure_all_modules_are_available_in_documentation(python_modules, docum
         )
 
 
-def test_ensure_all_documented_modules_are_from_this_library(python_modules, documented_modules):
-    documented_modules_not_in_source = documented_modules - python_modules
-    assert not documented_modules_not_in_source, \
-        'The following modules are available in documentation but not in source code. ' \
-        'Remove them from documentation or make sure that the code is available: {}'.format(
-            ', '.join(sorted(documented_modules_not_in_source)),
+@pytest.mark.parametrize(
+    'backward_incompatibility_tester',
+    BACKWARD_INCOMPATIBILITY_TESTERS,
+)
+def test_backward_incompatibility_testers_are_not_failing(backward_incompatibility_tester):
+    try:
+        check_call(
+            [sys.executable, backward_incompatibility_tester],
+            cwd=os.path.dirname(backward_incompatibility_tester),
         )
+    except CalledProcessError:  # pragma: no cover
+        pytest.fail('{} has failed to run.'.format(backward_incompatibility_tester))
