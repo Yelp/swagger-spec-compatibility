90f3ac22c5fac217296340b7c49d5a5e088fb99b
Samuele Maci <macisamuele@gmail.com>
2020-02-13 21:21:32 +0100

    Implement RES-E004: AddedNullablePropertyInResponse rule

--- swagger_spec_compatibility/rules/added_nullable_property_in_response.py
+++ swagger_spec_compatibility/rules/added_nullable_property_in_response.py
@@ -11,6 +11,10 @@ from swagger_spec_compatibility.rules.common import BaseRule
 from swagger_spec_compatibility.rules.common import Level
 from swagger_spec_compatibility.rules.common import RuleType
 from swagger_spec_compatibility.rules.common import ValidationMessage
+from swagger_spec_compatibility.util import is_path_in_top_level_paths
+from swagger_spec_compatibility.walkers import format_path
+from swagger_spec_compatibility.walkers.changed_xnullable import ChangedXNullableDifferWalker
+from swagger_spec_compatibility.walkers.response_paths import ResponsePathsWalker
 
 
 class AddedNullablePropertyInResponse(BaseRule):
@@ -24,4 +28,11 @@ class AddedNullablePropertyInResponse(BaseRule):
     @classmethod
     def validate(cls, left_spec, right_spec):
         # type: (Spec, Spec) -> typing.Iterable[ValidationMessage]
-        raise NotImplementedError()
+        response_paths = ResponsePathsWalker(left_spec, right_spec).walk()
+
+        for nullable_values_diff in ChangedXNullableDifferWalker(left_spec, right_spec).walk():
+            if not nullable_values_diff.mapping.new:
+                continue
+            if not is_path_in_top_level_paths(response_paths, nullable_values_diff.path):
+                continue
+            yield cls.validation_message(format_path(nullable_values_diff.path))
